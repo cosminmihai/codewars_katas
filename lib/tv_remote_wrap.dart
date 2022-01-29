c2f1d60c9434c302ab0aac0d979a082844e80ead
Cosmin <atok3th@gmail.com>
2022-01-29 14:21:37 +0200

    Solve "Tv remote(Wrap)"
    https://www.codewars.com/kata/5b2c2c95b6989da552000120/dart

--- lib/tv_remote_wrap.dart
+++ lib/tv_remote_wrap.dart
@@ -47,6 +47,8 @@
 // s => r-s-OK = 2
 // Answer = 6 + 12 + 4 + 2 + 5 + 6 + 6 + 6 + 2 = 49
 
+import 'dart:math';
+
 import 'package:test/test.dart';
 
 void main() {
@@ -54,175 +56,89 @@ void main() {
     expect(tvRemote('Code Wars'), equals(49));
   });
 
-/*  test('Lower', () {
-    expect(tv_remote('does'), equals(16));
-    expect(tv_remote('your'), equals(21));
-    expect(tv_remote('solution'), equals(33));
-    expect(tv_remote('work'), equals(18));
-    expect(tv_remote('for'), equals(12));
-    expect(tv_remote('these'), equals(27));
-    expect(tv_remote('words'), equals(23));
+  test('Lower', () {
+    expect(tvRemote('does'), equals(16));
+    expect(tvRemote('your'), equals(21));
+    expect(tvRemote('solution'), equals(33));
+    expect(tvRemote('work'), equals(18));
+    expect(tvRemote('for'), equals(12));
+    expect(tvRemote('these'), equals(27));
+    expect(tvRemote('words'), equals(23));
   });
 
   test('Upper', () {
-    expect(tv_remote('DOES'), equals(19));
-    expect(tv_remote('YOUR'), equals(22));
-    expect(tv_remote('SOLUTION'), equals(34));
-    expect(tv_remote('WORK'), equals(19));
-    expect(tv_remote('FOR'), equals(15));
-    expect(tv_remote('THESE'), equals(28));
-    expect(tv_remote('WORDS'), equals(24));
+    expect(tvRemote('DOES'), equals(19));
+    expect(tvRemote('YOUR'), equals(22));
+    expect(tvRemote('SOLUTION'), equals(34));
+    expect(tvRemote('WORK'), equals(19));
+    expect(tvRemote('FOR'), equals(15));
+    expect(tvRemote('THESE'), equals(28));
+    expect(tvRemote('WORDS'), equals(24));
   });
-
   test('Mixed', () {
-    expect(tv_remote('Does'), equals(28));
-    expect(tv_remote('Your'), equals(33));
-    expect(tv_remote('Solution'), equals(45));
-    expect(tv_remote('Work'), equals(26));
-    expect(tv_remote('For'), equals(20));
-    expect(tv_remote('These'), equals(35));
-    expect(tv_remote('Words'), equals(31));
-  });*/
+    expect(tvRemote('Does'), equals(28));
+    expect(tvRemote('Your'), equals(33));
+    expect(tvRemote('Solution'), equals(45));
+    expect(tvRemote('Work'), equals(26));
+    expect(tvRemote('For'), equals(20));
+    expect(tvRemote('These'), equals(35));
+    expect(tvRemote('Words'), equals(31));
+  });
 }
 
-const int _okPressed = 1;
+List<List<String>> keyboard = <List<String>>[
+  <String>['a', 'b', 'c', 'd', 'e', '1', '2', '3'],
+  <String>['f', 'g', 'h', 'i', 'j', '4', '5', '6'],
+  <String>['k', 'l', 'm', 'n', 'o', '7', '8', '9'],
+  <String>['p', 'q', 'r', 's', 't', '.', '@', '0'],
+  <String>['u', 'v', 'w', 'x', 'y', 'z', '_', '/'],
+  <String>['aA', ' ', '-1', '-1', '-1', '-1', '-1', '-1'],
+];
 
 int tvRemote(String word) {
   int totalSteps = 0;
-  bool capitalActive = false;
+  bool isUpperCaseActive = false;
   String previousSymbol = keyboard[0][0];
 
-  for (int index = 0; index < word.length; index++) {
-    final String symbol = word[index];
-    if (previousSymbol == symbol) {
-      totalSteps += _okPressed;
-      continue;
+  for (final String symbol in word.split('')) {
+    if (!isUpperCaseActive && isCapitalLetter(symbol) || isUpperCaseActive && isLowerLetter(symbol)) {
+      const String shift = 'aA';
+      isUpperCaseActive = !isUpperCaseActive;
+      totalSteps += calculateSteps(previousSymbol, shift);
+      previousSymbol = shift;
     }
 
-    int steps;
-    if (isCapitalAndLetter(symbol)) {
-      steps = calculateSteps(symbol, 'aA') + (capitalActive ? 0 : 1);
-      capitalActive = true;
-    } else if (symbol == ' ') {
-      steps = calculateSteps(symbol, 'SP');
-    } else {
-      capitalActive =
-      steps = calculateSteps(previousSymbol, symbol);
-    }
-
-    totalSteps += steps + _okPressed;
+    totalSteps += calculateSteps(previousSymbol, symbol);
     previousSymbol = symbol;
   }
 
   return totalSteps;
 }
 
-// q -> 0 ==> q(4, 2), 0(4, 8) -> 2 steps with wrap
-//                             -> 6 steps without wrap
-// v -> @ ==> v(5, 2), @(4,7) -> 6 without wrap
-//                            -> 4 with wrap
-
-// Applies when both x or y are 2 steps from wall
-// v(5, 2) - 7(3, 6) => (5 - 3) + (2 - 6) <=> 2 + 4 = 6 => Correct
-// u(5, 1) - 3(8, 1) => ([|6 - 5|] + [|8 - 8|]) + ([|0 - 1|] + [|0 - 1|]) <=> 1 + 0 + 1 + 1 = 3 => Correct
-// v(5, 2) - @(4, 7) => (5 - 4) + ([|0 - 2|] + [|8 - 7|]) <=> 1 + 2 + 1 = 4 => Correct
-// r(4, 3) - 7(3, 6) => (4 - 3) + (|3 - 6|) <=> 1 + 3 = 4 => Correct
-
-// v(5, 2) - 5(2, 7) => ([|6 - 5|] + [|0 - 2|]) + ([|0 - 2|] + [|8 - 7|]) <=> 1 + 2 + 2 + 1 = 6 => Correct
-// v(4, 1) - 5(1, 6) => ([|5 - 4|] + [|0 - 1|]) + ([|0 - 1|] + [|7 - 6|]) <=> 1 + 1 + 1 + 1 => 4 =>
-// 'v', '5'
-int calculateSteps(String first, String second) {
-  final List<int> firstCoordinate = getCoordinates(first);
-  final List<int> secondCoordinate = getCoordinates(second);
-
-  final int x1 = firstCoordinate[0];
-  final int y1 = firstCoordinate[1];
-  final int x2 = secondCoordinate[0];
-  final int y2 = secondCoordinate[1];
-
-  // ([|6 - 5|] + [|8 - 8|])
-  int xResult;
-  int yResult;
-  if (isCloseToTopOrBottom(x1) && isCloseToTopOrBottom(x2)) {
-    xResult = (x1 > 4 ? (6 - x1).abs() : x1) + (x2 > 4 ? (6 - x2).abs() : x2);
-  } else {
-    xResult = (x1 - x2).abs();
-  }
-
-  if (isCloseToRightOrLeft(y1) && isCloseToRightOrLeft(y2)) {
-    yResult = (y1 > 6 ? (8 - y1).abs() : y1) + (y2 > 6 ? (8 - y2).abs() : y2);
-  } else {
-    yResult = (y1 - y2).abs();
-  }
-
-  return xResult + yResult;
+bool isCapitalLetter(String symbol) {
+  return symbol.startsWith(RegExp('[A-Z]'));
 }
 
-bool isCapitalAndLetter(String letter) {
-  const String letters = 'abcdeghijklmnopqrstuvwxyz';
-  return letters.contains(letter) && letter.toUpperCase() == letter;
+bool isLowerLetter(String symbol) {
+  return symbol.startsWith(RegExp('[a-z]'));
 }
 
-bool isCloseToTopOrBottom(int index) {
-  return index < 3 || index > 4;
-}
+int calculateSteps(String startingSymbol, String endingSymbol) {
+  final String firstLowerCase = startingSymbol == 'aA' ? startingSymbol : startingSymbol.toLowerCase();
+  final String secondLowerCase = endingSymbol == 'aA' ? endingSymbol : endingSymbol.toLowerCase();
+  final List<int> firstCoordinate = getCoordinates(firstLowerCase);
+  final List<int> secondCoordinate = getCoordinates(secondLowerCase);
 
-bool isCloseToRightOrLeft(int index) {
-  return index < 3 || index > 6;
+  final int rowDifference = (firstCoordinate[0] - secondCoordinate[0]).abs();
+  final int columnDifference = (firstCoordinate[1] - secondCoordinate[1]).abs();
+  final int rowSteps = min(rowDifference, 6 - rowDifference);
+  final int columnSteps = min(columnDifference, 8 - columnDifference);
+
+  return rowSteps + columnSteps + 1;
 }
 
 List<int> getCoordinates(String symbol) {
-  final int index = keyboard.indexOf(symbol);
-  return <int>[(index ~/ 8) + 1, (index % 8) + 1]; // [x, y]
+  final int row = keyboard.indexWhere((List<String> row) => row.contains(symbol));
+  final int column = keyboard[row].indexOf(symbol);
+  return <int>[row, column];
 }
-
-final List<String> keyboard = <String>[
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  '1',
-  '2',
-  '3',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  '4',
-  '5',
-  '6',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  '7',
-  '8',
-  '9',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  '.',
-  '@',
-  '0',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z',
-  '_',
-  '/',
-  'aA', // Shift
-  ' ',
-  // Empty cells:
-  '-1',
-  '-1',
-  '-1',
-  '-1',
-  '-1',
-];
